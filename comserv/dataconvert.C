efd53aaa7045f6b21add686161f12fbb1e2803a5
AresTao <wuzhantao0725@163.com>
2015-12-01 16:04:06 +0800

    update

--- comserv/dataconvert.C
+++ comserv/dataconvert.C
@@ -11,106 +11,106 @@ extern "C"
 
 char* itoa_character_set(TEncodingType type_uint)
 {
-	switch (type_uint)
-	{
-		case data_encoding_ascii:
-			return "ASCII";
-		case data_encoding_ucs2:
-			return "UCS-2BE";
-		case data_encoding_utf8:
-			return "UTF-8";
-		case data_encoding_gb18030:
-			return "GB18030";
-		case data_encoding_default:
-			return "GB18030";		
-		default:
-			return "UNKNOWN";
+    switch (type_uint)
+    {
+        case data_encoding_ascii:
+            return "ASCII";
+        case data_encoding_ucs2:
+            return "UCS-2BE";
+        case data_encoding_utf8:
+            return "UTF-8";
+        case data_encoding_gb18030:
+            return "GB18030";
+        case data_encoding_default:
+            return "GB18030";		
+        default:
+            return "UNKNOWN";
 
-	}
+    }
 }
 
 TEncodingType atoi_character_set(const char* type_char)
 {
-	if(type_char==NULL) return data_encoding_default;
-	char temp[64];
-	strcpy(temp,type_char);
-	if(strnotcasecmp(temp,"GB18030")==0)
-		return data_encoding_gb18030;
-	else if(strnotcasecmp(temp,"GBK")==0)
-		return data_encoding_gb18030;
-	else if(strnotcasecmp(temp,"UCS-2")==0)
-		return data_encoding_ucs2;
-	else if(strnotcasecmp(temp,"UTF-8")==0)
-		return data_encoding_utf8;
-	else if(strnotcasecmp(temp,"ASCII")==0)
-		return data_encoding_ascii;
-	else
-		return data_encoding_default;
-	
+    if(type_char==NULL) return data_encoding_default;
+    char temp[64];
+    strcpy(temp,type_char);
+    if(strnotcasecmp(temp,"GB18030")==0)
+        return data_encoding_gb18030;
+    else if(strnotcasecmp(temp,"GBK")==0)
+        return data_encoding_gb18030;
+    else if(strnotcasecmp(temp,"UCS-2")==0)
+        return data_encoding_ucs2;
+    else if(strnotcasecmp(temp,"UTF-8")==0)
+        return data_encoding_utf8;
+    else if(strnotcasecmp(temp,"ASCII")==0)
+        return data_encoding_ascii;
+    else
+        return data_encoding_default;
+
 }
 
 int dataEncodingConvert(TEncodingType srcEncoding,char* srcContent,const unsigned int srcLength,TEncodingType destEncoding,char* destContent,unsigned int &destLength)
 {
 #ifndef WIN32
-	iconv_t cd;					  /* conversion descriptor */
-	size_t ret_val;				  /* number of conversions */
-	size_t inbytesleft=srcLength;
-	size_t outbytesleft=destLength; 
+    iconv_t cd;					  /* conversion descriptor */
+    size_t ret_val;				  /* number of conversions */
+    size_t inbytesleft=srcLength;
+    size_t outbytesleft=destLength; 
 
-	if ((NULL==srcContent)||(NULL==destContent))
-	{
-		destLength=0;
-		return 0;
-	}
+    if ((NULL==srcContent)||(NULL==destContent))
+    {
+        destLength=0;
+        return 0;
+    }
 
-	/* Initiate conversion -- get conversion descriptor */
-	if ((cd = iconv_open(itoa_character_set(destEncoding), itoa_character_set(srcEncoding))) == (iconv_t)-1) 
-	{
-		perror("iconv_open() error:");
-		destLength=0;
-		return 0;
-	}
+    /* Initiate conversion -- get conversion descriptor */
+    if ((cd = iconv_open(itoa_character_set(destEncoding), itoa_character_set(srcEncoding))) == (iconv_t)-1) 
+    {
+        perror("iconv_open() error:");
+        destLength=0;
+        return 0;
+    }
 
-	ret_val = iconv(cd, &srcContent, &inbytesleft, &destContent, &outbytesleft);
+    ret_val = iconv(cd, &srcContent, &inbytesleft, &destContent, &outbytesleft);
 
-	if (ret_val<0)
-	{
-		perror("iconv() error:");
-		destLength=0;
-		return 0;
-	}
+    if (ret_val<0)
+    {
+        perror("iconv() error:");
+        destLength=0;
+        return 0;
+    }
 
-	if (iconv_close(cd)<0)
-	{
-		perror("iconv_close() error:");
-	}
+    if (iconv_close(cd)<0)
+    {
+        perror("iconv_close() error:");
+    }
 
-	destLength = destLength-outbytesleft;
-	return 1;
+    destLength = destLength-outbytesleft;
+    return 1;
 #else
-	return 0;
+    return 0;
 #endif
 }
 
 
 int dataEncodingPrint(char* srcBuf, int srcLen, TEncodingType encoding, char* destBuf, int& dstLen)
 {
-	switch(encoding)
-	{
-		case data_encoding_ucs2:
-			if( srcLen%2 > 0 ) return 0;
-			dstLen=0;
-			for(int i=0;i<srcLen/2;i++)
-			{
-				sprintf(destBuf,"%%u%02x%02x",(unsigned char)srcBuf[i],(unsigned char)srcBuf[i+1]);
-				i=i+2;
-				dstLen+=6;
-			}
-			return 1;
-		default:
-			return 0;
-	}
-	return 0;
+    switch(encoding)
+    {
+        case data_encoding_ucs2:
+            if( srcLen%2 > 0 ) return 0;
+            dstLen=0;
+            for(int i=0;i<srcLen/2;i++)
+            {
+                sprintf(destBuf,"%%u%02x%02x",(unsigned char)srcBuf[i],(unsigned char)srcBuf[i+1]);
+                i=i+2;
+                dstLen+=6;
+            }
+            return 1;
+        default:
+            return 0;
+    }
+    return 0;
 }
 
 
--- comserv/db.C
+++ comserv/db.C
@@ -8,46 +8,46 @@ static INT DBConnectionID=0;
 
 CDB::CDB()
 {
-	m_iDBCnt = 1;
-	m_pSelectResult = new TSelectResult(); //整个程序运行期内使用此同一指针.
-	m_pSelectResult->pRows = NULL;
-	m_pSelectResult	-> rowNum = 0;
-	m_bConnFlag = FALSE;
-	m_bTransaction = FALSE;
+    m_iDBCnt = 1;
+    m_pSelectResult = new TSelectResult(); //整个程序运行期内使用此同一指针.
+    m_pSelectResult->pRows = NULL;
+    m_pSelectResult	-> rowNum = 0;
+    m_bConnFlag = FALSE;
+    m_bTransaction = FALSE;
 
-	m_iProcRowCount = 0;
+    m_iProcRowCount = 0;
 
-	strcpy(m_pcErrMsg ,"There is no error");
-	strcpy(m_pcDBsv,(CHAR* )"");
-	strcpy(m_pcUsr,(CHAR* )"");
-	strcpy(m_pcPwd,(CHAR* )"");
+    strcpy(m_pcErrMsg ,"There is no error");
+    strcpy(m_pcDBsv,(CHAR* )"");
+    strcpy(m_pcUsr,(CHAR* )"");
+    strcpy(m_pcPwd,(CHAR* )"");
 
-	mConnectionID=DBConnectionID+1;
+    mConnectionID=DBConnectionID+1;
 
-	return;
+    return;
 }
 
 CDB::~CDB()
 {
-	freeResult();
-	delete m_pSelectResult;
-	m_pSelectResult = NULL;
+    freeResult();
+    delete m_pSelectResult;
+    m_pSelectResult = NULL;
 }
 
 INT CDB::execSQL(const char* pcSQLStmt)
 {
-   timeval execBeginTime;
+    timeval execBeginTime;
 
-   int re;
+    int re;
 
-   m_iQueryFlag = 0;
-   re = execSQL1(pcSQLStmt);
-   return re;
+    m_iQueryFlag = 0;
+    re = execSQL1(pcSQLStmt);
+    return re;
 }
 
 INT CDB::execSQLFmt(const char *format, ...)
 {
-	INT re;
+    INT re;
     va_list ap;
     va_start(ap,format);
     re = execSQL2(format,ap);
@@ -57,247 +57,247 @@ INT CDB::execSQLFmt(const char *format, ...)
 
 CHAR* CDB::getErrMsg()
 {
-	return m_pcErrMsg;
+    return m_pcErrMsg;
 }
 
 void CDB::freeRowMem(struct slctRslt* pstruSlctRslt)
 {
-	struct slctRslt* p = NULL;
-	while (pstruSlctRslt != NULL)
-	{
-		p = pstruSlctRslt;
-		pstruSlctRslt = pstruSlctRslt->pstruNext;
-		delete p->pvResult;
-		delete p;
-		p = NULL;
-	}
+    struct slctRslt* p = NULL;
+    while (pstruSlctRslt != NULL)
+    {
+        p = pstruSlctRslt;
+        pstruSlctRslt = pstruSlctRslt->pstruNext;
+        delete p->pvResult;
+        delete p;
+        p = NULL;
+    }
 }
 
 void CDB::freeResult()
 {
-	TRow *row = m_pSelectResult->pRows;
-	while(row!=NULL)
-	{
-		TRow *p = row;
-		row = row->next;
-		delete p;
-	}
-	m_pSelectResult->pRows=NULL; 
-	m_pSelectResult	-> rowNum = 0;
-	return;
+    TRow *row = m_pSelectResult->pRows;
+    while(row!=NULL)
+    {
+        TRow *p = row;
+        row = row->next;
+        delete p;
+    }
+    m_pSelectResult->pRows=NULL; 
+    m_pSelectResult	-> rowNum = 0;
+    return;
 }
 
 CDB* initDB(CDB::DBType dbType, const CHAR* dbUser, const CHAR* dbPasswd, const CHAR* dbName, const CHAR* dbHost, INT dbPort)
 {
-	INT rt = 0;
-	CDB* uniDB;
-	if(dbType == CDB::DB_MYSQL)
-	{
-		uniDB = new CDBMysql();
-		UniINFO("new DBMySql ok.");
-		rt = uniDB->connDB(dbUser, dbPasswd, dbName, dbHost, dbPort);
-	}
-/*	else if(dbType == CDB::DB_REDIS)
-	{
-		uniDB = new CDBRedis();
-		UniINFO("new DBRedis ok.");
-		rt = uniDB->connDB(dbUser, dbPasswd, dbName, dbHost, dbPort);
-	}*/
-	else if(dbType == CDB::DB_REDIS)
-	{
-		uniDB = new CDBRedisCluster();
-		UniINFO("new DBRedisCluster ok.");
-		rt = uniDB->connDB(dbUser, dbPasswd, dbName, dbHost, dbPort);
-                if (rt < 0)
-                    UniERROR("DBRedisCluster setup error");
-	}
-	else
-	{
-		UniERROR("DBType %d is not supported yet. The program will exit now...", dbType);
-		exit(0);
-	}
+    INT rt = 0;
+    CDB* uniDB;
+    if(dbType == CDB::DB_MYSQL)
+    {
+        uniDB = new CDBMysql();
+        UniINFO("new DBMySql ok.");
+        rt = uniDB->connDB(dbUser, dbPasswd, dbName, dbHost, dbPort);
+    }
+    /*	else if(dbType == CDB::DB_REDIS)
+        {
+        uniDB = new CDBRedis();
+        UniINFO("new DBRedis ok.");
+        rt = uniDB->connDB(dbUser, dbPasswd, dbName, dbHost, dbPort);
+        }*/
+    else if(dbType == CDB::DB_REDIS)
+    {
+        uniDB = new CDBRedisCluster();
+        UniINFO("new DBRedisCluster ok.");
+        rt = uniDB->connDB(dbUser, dbPasswd, dbName, dbHost, dbPort);
+        if (rt < 0)
+            UniERROR("DBRedisCluster setup error");
+    }
+    else
+    {
+        UniERROR("DBType %d is not supported yet. The program will exit now...", dbType);
+        exit(0);
+    }
 
-	if(rt>0)
-	{
-		UniINFO("Connecting db %s@%s:%s succeed. dbType:%d ", dbName, dbUser, dbHost, dbType);
-	}
-	else
-	{
-		UniERROR("Connecting db %s@%s:%s error. dbType:%d, Error info:%s ",dbName, dbUser, dbHost, dbType,
-				 uniDB->getErrMsg() );
-		return NULL;
-	}
+    if(rt>0)
+    {
+        UniINFO("Connecting db %s@%s:%s succeed. dbType:%d ", dbName, dbUser, dbHost, dbType);
+    }
+    else
+    {
+        UniERROR("Connecting db %s@%s:%s error. dbType:%d, Error info:%s ",dbName, dbUser, dbHost, dbType,
+                uniDB->getErrMsg() );
+        return NULL;
+    }
 
-	return uniDB;
+    return uniDB;
 }
 
 
 void CDB::printState(CStr& str, int indent)
 {
-   str.fCat("(%s)", type());
+    str.fCat("(%s)", type());
 }
 
 Field* CDB::selectOneField(const char* sql)
 {
-   int re = execSql(sql);
-   if(re != 1)
-   {
-      UniERROR("EXEC SQL: '%s' failed, %s", sql, getErrMsg());
-      return NULL;
-   }
-   TSelectResult* selectResult = getSelectResult();
-   if(selectResult == NULL)
-   {
-      UniERROR("EXEC SQL: '%s' result is NULL", sql);
-      return NULL;
-   }
-   if(selectResult->fieldNum < 1 || selectResult->pRows == NULL)
-   {
-      return NULL;
-   }
-   if(selectResult->pRows->arrayField[0].isNull)
-   {
-      return NULL;
-   }
-   return &(selectResult->pRows->arrayField[0]);
+    int re = execSql(sql);
+    if(re != 1)
+    {
+        UniERROR("EXEC SQL: '%s' failed, %s", sql, getErrMsg());
+        return NULL;
+    }
+    TSelectResult* selectResult = getSelectResult();
+    if(selectResult == NULL)
+    {
+        UniERROR("EXEC SQL: '%s' result is NULL", sql);
+        return NULL;
+    }
+    if(selectResult->fieldNum < 1 || selectResult->pRows == NULL)
+    {
+        return NULL;
+    }
+    if(selectResult->pRows->arrayField[0].isNull)
+    {
+        return NULL;
+    }
+    return &(selectResult->pRows->arrayField[0]);
 }
 
 int CDB::count(const char* tableName, const char* condition)
 {
-   CStr sql;
-   sql << "select count(*) from " << tableName;
-   if(condition != NULL)
-      sql << "  where " << condition;
+    CStr sql;
+    sql << "select count(*) from " << tableName;
+    if(condition != NULL)
+        sql << "  where " << condition;
 
-   struct Field* field = selectOneField(sql.c_str());
-   if(field == NULL)
-   {
-      UniERROR("EXEC SQL: '%s', no valid field selected ", sql.c_str());
-      return -1;
-   }
+    struct Field* field = selectOneField(sql.c_str());
+    if(field == NULL)
+    {
+        UniERROR("EXEC SQL: '%s', no valid field selected ", sql.c_str());
+        return -1;
+    }
 
-   int count;
-   switch(field->type)
-   {
-   case T_INT:
-      count = field->value.intValue;
-      break;
-   case T_UINT:
-      count = field->value.uintValue;
-      break;
-   case T_LONGLONG:
-      count = field->value.longlongValue;
-      break;
-   case T_FLOAT:
-      count = (int)field->value.floatValue;
-      break;
-   default:
-      UniERROR("EXEC SQL: '%s' field type is %d", sql.c_str(), field->type);
-      return -1;
-   }
-   return count;
+    int count;
+    switch(field->type)
+    {
+        case T_INT:
+            count = field->value.intValue;
+            break;
+        case T_UINT:
+            count = field->value.uintValue;
+            break;
+        case T_LONGLONG:
+            count = field->value.longlongValue;
+            break;
+        case T_FLOAT:
+            count = (int)field->value.floatValue;
+            break;
+        default:
+            UniERROR("EXEC SQL: '%s' field type is %d", sql.c_str(), field->type);
+            return -1;
+    }
+    return count;
 }
 
 int CDB::getFieldInt(struct Field* field, int& result)
 {
-   switch(field->type)
-   {
-   case T_INT:
-      result = field->value.intValue;
-      return 1;
-   case T_UINT:
-      result = field->value.uintValue;
-      return 1;
-   case T_LONGLONG:
-      result = field->value.longlongValue;
-      return 1;
-   case T_FLOAT:
-      result = (int)field->value.floatValue;
-      return 1;
-   default:
-      return 0;
-   }
+    switch(field->type)
+    {
+        case T_INT:
+            result = field->value.intValue;
+            return 1;
+        case T_UINT:
+            result = field->value.uintValue;
+            return 1;
+        case T_LONGLONG:
+            result = field->value.longlongValue;
+            return 1;
+        case T_FLOAT:
+            result = (int)field->value.floatValue;
+            return 1;
+        default:
+            return 0;
+    }
 }
 
 int CDB::selectInt(const char* sql, int& result)
 {
-   struct Field* field = selectOneField(sql);
-   if(field == NULL)
-      return 0;
-   int re = getFieldInt(field, result);
-   if(!re)
-      UniERROR("EXEC SQL: '%s' result type is %d, not int", sql, field->type);
-   return re;
+    struct Field* field = selectOneField(sql);
+    if(field == NULL)
+        return 0;
+    int re = getFieldInt(field, result);
+    if(!re)
+        UniERROR("EXEC SQL: '%s' result type is %d, not int", sql, field->type);
+    return re;
 }
 
 int CDB::selectStr(const char* sql, CStr& result)
 {
-   struct Field* field = selectOneField(sql);
-   if(field == NULL)
-      return 0;
+    struct Field* field = selectOneField(sql);
+    if(field == NULL)
+        return 0;
 
-   if(field->type == T_STRING)
-   {
-      result = field->value.stringValue;
-      return 1;
-   }
-   UniERROR("EXEC SQL: '%s' result type is %d, not string", sql, field->type);
-   return 0;
+    if(field->type == T_STRING)
+    {
+        result = field->value.stringValue;
+        return 1;
+    }
+    UniERROR("EXEC SQL: '%s' result type is %d, not string", sql, field->type);
+    return 0;
 }
 
 int CDB::selectIntList(const char* sql, CList<int>& result)
 {
-   int re = execSql(sql);
-   if(re != 1)
-   {
-      UniERROR("EXEC SQL: '%s' failed, %s", sql, getErrMsg());
-      return 0;
-   }
-   TSelectResult* selectResult = getSelectResult();
-   if(selectResult != NULL && selectResult->fieldNum > 0)
-   {
-      TRow *row = m_pSelectResult->pRows;
-      while(row!=NULL)
-      {
-         struct Field* field = &(row->arrayField[0]);
-         int value;
-         int re = getFieldInt(field, value);
-         if(!re)
-         {
-            UniERROR("EXEC SQL: '%s' result type is %d, not int", sql, field->type);
-            return 0;
-         }
-         result.insert(value);
-         row = row->next;
-      }
-   }
-   return 1;
+    int re = execSql(sql);
+    if(re != 1)
+    {
+        UniERROR("EXEC SQL: '%s' failed, %s", sql, getErrMsg());
+        return 0;
+    }
+    TSelectResult* selectResult = getSelectResult();
+    if(selectResult != NULL && selectResult->fieldNum > 0)
+    {
+        TRow *row = m_pSelectResult->pRows;
+        while(row!=NULL)
+        {
+            struct Field* field = &(row->arrayField[0]);
+            int value;
+            int re = getFieldInt(field, value);
+            if(!re)
+            {
+                UniERROR("EXEC SQL: '%s' result type is %d, not int", sql, field->type);
+                return 0;
+            }
+            result.insert(value);
+            row = row->next;
+        }
+    }
+    return 1;
 }
 
 int CDB::selectStrList(const char* sql, CList<CStr>& result)
 {
-   int re = execSql(sql);
-   if(re != 1)
-   {
-      UniERROR("EXEC SQL: '%s' failed, %s", sql, getErrMsg());
-      return 0;
-   }
-   TSelectResult* selectResult = getSelectResult();
-   if(selectResult != NULL && selectResult->fieldNum > 0)
-   {
-      TRow *row = m_pSelectResult->pRows;
-      while(row!=NULL)
-      {
-         struct Field* field = &(row->arrayField[0]);
-         if(field->type != T_STRING)
-         {
-            UniERROR("EXEC SQL: '%s' result type is %d, not string", sql, field->type);
-            return 0;
-         }
-         CStr value = field->value.stringValue;
-         result.insert(value);
-         row = row->next;
-      }
-   }
-   return 1;
+    int re = execSql(sql);
+    if(re != 1)
+    {
+        UniERROR("EXEC SQL: '%s' failed, %s", sql, getErrMsg());
+        return 0;
+    }
+    TSelectResult* selectResult = getSelectResult();
+    if(selectResult != NULL && selectResult->fieldNum > 0)
+    {
+        TRow *row = m_pSelectResult->pRows;
+        while(row!=NULL)
+        {
+            struct Field* field = &(row->arrayField[0]);
+            if(field->type != T_STRING)
+            {
+                UniERROR("EXEC SQL: '%s' result type is %d, not string", sql, field->type);
+                return 0;
+            }
+            CStr value = field->value.stringValue;
+            result.insert(value);
+            row = row->next;
+        }
+    }
+    return 1;
 }
