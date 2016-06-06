09f636cfdf351903b4ffdef533ecef32e319f892
AresTao <wuzhantao0725@163.com>
2016-06-06 16:44:11 +0800

    delete chinese note

--- include/comserv/socket.h
+++ include/comserv/socket.h
@@ -10,12 +10,12 @@
 #include <sys/time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h> //include inet_addr()  
+#include <arpa/inet.h>   
 #include <netdb.h>
 #include <unistd.h>
 #include <signal.h>
 #else
-#include <winsock2.h> //include timeval
+#include <winsock2.h> 
 #endif
 
 #include <fcntl.h>
@@ -33,38 +33,23 @@
 #define ZERO            0x00
 #define ONE             0xff
 
-//Modified by Long Xiangming. 2007.9.3
-//Modified by Long Xiangming. 2007.9.16
-//#define      SendBufSize      MaxMsgLength //80K
-#define SendBufSize         4096000 //2048000 //4M.假设一个消息平均为1K,则可以同时发送4000个消息。
-//#define       RecvBufSize      8192
-//#define       RecvBufSize      MaxMsgLength //80k
+#define SendBufSize         4096000 
 #define RecvBufSize          SendBufSize //
 #define LOCAL_PORT_NUM  18
 #define SOCKET_CODE_MAXLENGTH   20476
-//typedef      I32  TCommState;
-/*
-#define      tc_primitive      0x00
-#define    shutdown_request   0x01
-#define    shutdown_indication   0x02
-#define      routine_check      0x03
-#define      sms_management      0x04
-#define      ctrl_management     0x05
-*/
-
-typedef enum SStateMachine /*搜寻包头自动机状态 */
+typedef enum SStateMachine 
 {
-    Wait =0, /*初始状态 */
-    Begin =1, /*读到‘0’*/
-    Appear =2, /*读到‘01’ */
-    Prepare =3, /*读到‘0111111’ */
-    Found =4 /*读到‘01111110’ */
+    Wait =0, 
+    Begin =1, 
+    Appear =2, 
+    Prepare =3, 
+    Found =4 
 } TStateMachine;
 
-typedef enum SWrState /*写状态 */
+typedef enum SWrState 
 {
-    NoMsgInQueue=0, /*消息队列中无消息 */
-    MsgInQueue=1 /*消息队列中有消息 */
+    NoMsgInQueue=0, 
+    MsgInQueue=1 
 } TWrState;
 
 enum TCommRole
@@ -76,13 +61,13 @@ enum TCommRole
 
 typedef struct SQueueItem QueueItem;
 
-struct SQueueItem /*消息队列中的消息项*/
+struct SQueueItem 
 
 {
-    CHAR* pcMsg; /*消息项的内容指针 */
-    CHAR* pcMsgPointer; /*移动指针*/
-    UINT iLengthToWrite;/*要写的字节数*/
-    QueueItem* pNext; /*消息项的下一项指针 */
+    CHAR* pcMsg; 
+    CHAR* pcMsgPointer; 
+    UINT iLengthToWrite;
+    QueueItem* pNext; 
 };
 
 _CLASSDEF(CSocket)
@@ -93,54 +78,19 @@ _CLASSDEF(CSocket)
     public:
         CSocket();
         virtual ~CSocket();
-        //返回值：
-        //-1:失败
-        // 0:监听成功，未连接
-        // 1:打开连接成功
-        INT openServer(CHAR* pcServerAddr, INT iServerPort, BOOL bNoBlock=TRUE); //打开服务端socket
+	INT openServer(CHAR* pcServerAddr, INT iServerPort, BOOL bNoBlock=TRUE); 
 
-        //打开客户端socket. 
-        //Modified By Long Xiangming. 2007.03.29
-        //增加iClientPort参数。如果等于0,则由操作系统选定客户端端口；如果不为0,则使用此客户端端口
-        //Added by Long Xiangming. 2007.11.09 (PSA-SMPP模块用到。当本机存在多个IP时用到。上海短信中心上线时发现此问题) 
-        //增加pcClientAddr参数。如果等于NULL,则由操作系统选定一个IP地址;如果不为NULL,则使用此客户端IP
-        virtual BOOL openClient(CHAR* pcServerAddr, INT iServerPort,
+	virtual BOOL openClient(CHAR* pcServerAddr, INT iServerPort,
                 CHAR* pcClientAddr=NULL, INT iClientPort=0,
                 struct timeval *timeout=NULL);
 
-        //added by Long Xiangming. 2006.03.02
-        virtual BOOL reAccept();//重新监听连接
-        //added by Long Xiangming. 2006.05.31
-        virtual BOOL reConnect();//重新连接到Server
+        virtual BOOL reAccept();
+        virtual BOOL reConnect();
 
         BOOL hasBufferedCode();
-        /*
-         * return result:
-         * 1：成功读到所需数据
-         * 0：读到了0个字节. 对方socket已断开
-         * -1：socket错误
-         * -2：读到字节数少于要求数
-         * -3：不再使用（原意为搜索包头失败）
-         * -4：code错误，未分配地址空间
-         * -5：直接读取（无定界）时超长
-         * -9：socket没有建立
-         *
-         *   只有返回1表示读取正确。
-         *   如果返回0或-9，应该关闭或移除本socket，并进行重新连接或监听。
-         其他错误不用管。
-         */
-        virtual INT recvCode(CCode& code); //接收消息主函数
+	virtual INT recvCode(CCode& code); 
 
-        // Return result：
-        //      INT
-        //       1-----发送成功
-        //       -1----写socket失败
-        //       -2----向socket写入的字节数与期望值不相同(保存到sendBuff中)
-        //       -3----(不用)
-        //       -5----写socket sendbuff 满
-        //        0 ---- 对端socket主动关闭或本端Socket已经主动关闭(m_socketState==CLOSE)。
-        //       -9----对端Socket连接已经断开，或者物理链路故障
-        virtual INT sendCode(CCode &code); //发送消息的主函数
+	virtual INT sendCode(CCode &code); 
 
         BOOL isSocketStateClose() {
             return (m_socketState==CLOSE) ? TRUE : FALSE;
@@ -148,15 +98,14 @@ _CLASSDEF(CSocket)
 
         void setSocketStateClose();
 
-        //增加一个虚函数，便于使用多态。
         virtual BOOL setListenPort(CHAR* pcListenAddr, INT iListenPort);
 
-        BOOL closeSocketConnection(); //关闭socket
-        BOOL closeSocket(); //关闭socket
+        BOOL closeSocketConnection(); 
+        BOOL closeSocket(); 
 
-        BOOL isNoBlock(); //取得socket文件当前状态是否是阻塞模式
+        BOOL isNoBlock(); 
 
-        void setNoBlock(BOOL bIsNoBlock); //设置socket是否以非阻塞模式进行通信
+        void setNoBlock(BOOL bIsNoBlock); 
 
         void setNeedFindHead(BOOL isNeedFindHead) {
             m_bIsNeedFindHead=isNeedFindHead;
@@ -170,11 +119,11 @@ _CLASSDEF(CSocket)
         sockaddr_in& getClientSockAddr() {
             return client;
         }
-        CHAR* c_str_remote(); //得到对端的地址:端口
+        CHAR* c_str_remote(); 
 
-        void setSendBufSize(INT iSendBufSize);//设置发送缓冲区大小
+        void setSendBufSize(INT iSendBufSize);
 
-        void setRecvBufSize(INT iRecvBufSize);//设置接收缓冲区大小
+        void setRecvBufSize(INT iRecvBufSize);
 
 
         TCommRole getCommRole() {
@@ -193,45 +142,43 @@ _CLASSDEF(CSocket)
             m_shortConnectionMode = 1;
         }
 
-        BOOL isReconnected(); //仅仅当以前连接成功且调用了reconnect成功之后，才为TRUE
+        BOOL isReconnected(); 
 
         void serverInfo(CStr*);
         void clientInfo(CStr*);
         void connectionInfo(CStr*);
     protected:
-        /////////////////////////////////////////////// 
-        INT getFlag(); //取得socket文件状态标志
-        INT peekSocket(); //通过从socket中读取一个字节，来判断socket状态
+        INT getFlag(); 
+        INT peekSocket(); 
         void clearBuffer();
         int _readFromBuff(void *dst, const void *src, unsigned int length);
         int _recvSocketIntoBuff(void *dst, const void *src, unsigned int length);
-        INT recvCodeByFindHead(CCode& code); //需要处理消息定界符
-        INT recvCodeDirect(CCode& code); //不需要处理消息定界符，直接接收
+        INT recvCodeByFindHead(CCode& code);
+        INT recvCodeDirect(CCode& code); 
 
 
-        virtual void pack(CCode &code, CCode& Packet); //打包
+        virtual void pack(CCode &code, CCode& Packet); 
         BOOL hasUnsentCode();
-        INT  preSendProcess(CCode &code);//added by Yubo Chow on Oct 10, 2008
-        INT  sendUnsentCode(CCode &code);//added by ybzhou.  2008.10.10
-        INT  sendMessage(CCode &code); //发送消息
+        INT  preSendProcess(CCode &code);
+        INT  sendUnsentCode(CCode &code);
+        INT  sendMessage(CCode &code); 
         INT backupToSendBuff(CHAR* codeContent,UINT codeLength,INT codeFlag,CHAR* funcName);
-        //added by lxm. 2008.10.13
-        int  _sendBuffToSocket(void *dst, const void *src, unsigned int length);//added by lxm. 2008.10.14
+        
+        int  _sendBuffToSocket(void *dst, const void *src, unsigned int length);
         int _writeToBuff(void *dst, const void *src, unsigned int length);
 
-        //执行真正的SET动作。
-        void _setSendBufSize(INT iSendBufSize);//设置发送缓冲区大小
-        void _setRecvBufSize(INT iRecvBufSize);//设置接收缓冲区大小
+        void _setSendBufSize(INT iSendBufSize);
+        void _setRecvBufSize(INT iRecvBufSize);
 
-        void setClientLinger(BOOL isLinger); //设置客户端Linger
+        void setClientLinger(BOOL isLinger); 
 
-        void setServerLinger(BOOL isLinger); //设置服务端Linger
+        void setServerLinger(BOOL isLinger); 
 
-        void setClientAlive(BOOL isAlive); //设置客户端Alive
+        void setClientAlive(BOOL isAlive); 
 
-        void setServerAlive(BOOL isAlive); //设置服务端Alive//modified by lxm. 2008.12.01
+        void setServerAlive(BOOL isAlive); 
 
-        struct timeval m_conAttemptTimeout; //连接尝试超时时间。client 尝试连接 server 时的超时时间
+        struct timeval m_conAttemptTimeout;
         virtual BOOL openClientResult(int rt);
         virtual BOOL socketInit(CHAR* pcServerAddr, INT iServerPort,
                 CHAR* pcClientAddr, INT iClientPort);
@@ -239,57 +186,47 @@ _CLASSDEF(CSocket)
         struct sockaddr_in server; //server internet addr.
         struct sockaddr_in client; //client internet addr.
         CHAR m_localServerAddr[30];
-        CHAR m_localClientAddr[30]; //added by Long Xiangming. 2007.11.09
+        CHAR m_localClientAddr[30]; 
         CHAR m_remoteServerAddr[30];
         INT m_localServerPort;
         INT m_remoteServerPort;
         INT m_localClientPort;
         TCommState m_socketState; //socket state : OPEN / CLOSE
-        TCommRole m_socketRole; //角色:作为Server还是Client
+        TCommRole m_socketRole; 
         BOOL m_bIsNeedFindHead;
 
-        //WrState       WriteState; //发送时判断消息队列中有无消息 
 
 #ifndef WIN32
-        struct sigaction act; //传送信号处理器信息的结构
-        socklen_t m_iSocklen; //socket通信地址结构长度
+        struct sigaction act; 
+        socklen_t m_iSocklen; 
 #else
-        INT m_iSocklen; //socket通信地址结构长度
+        INT m_iSocklen;
 #endif
 
-        INT m_iSockWait; //accept() used.
-        TBuffer<CSocket>* pSendMsgBuf;//modified by lxm. 2008.10.13
+        INT m_iSockWait; 
+        TBuffer<CSocket>* pSendMsgBuf;
         TBuffer<CSocket> *pRecvMsgBuf;
         BOOL recvBufHasMessage;
-        //QueueItem*    pSendQueueHead; //发送队列头指针
-
-        //QueueItem*    pSendQueueTail; //发送队列尾指针
-        BOOL m_addrReuseFlag; //地址重用标记。 //added by Long Xiangming.2006.06.30
-        //缺省为1
+        BOOL m_addrReuseFlag; 
         BOOL _reserved1; 
 
-        BOOL m_bNoBlock; //调用Send或Recv时的阻塞模式，缺省为True,为非阻塞
+        BOOL m_bNoBlock; 
 
-        //CCode     PrePackage;    //发送之前的插入0预处理   
+        CCode Package; 
 
-        CCode Package; //发送之前的打包预处理
-
-        CCode RecvCode; //接收到的code处理
+        CCode RecvCode; 
 
         CCode *codeToReceive;
 
-        //added by Long Xiangming. 2007.9.19
-        //socket buff 可以设置。
         UINT m_socketSendBufSize;
         UINT m_socketRecvBufSize;
 
-        //
-        CHAR m_remoteAddrStr[128];//c_str_remote返回
+        CHAR m_remoteAddrStr[128];
 
-        UINT m_connectedCounter; //当客户端时，连接成功的次数
+        UINT m_connectedCounter; 
 
         fd_set fd_writeset;
-        BOOL b_writeNeedSelect;//added by Yubo Chow on Oct 10, 2008
+        BOOL b_writeNeedSelect;
         timeval m_resendSelectWaitTime;
         int m_shortConnectionMode;
 };
@@ -306,7 +243,6 @@ class CUri
         bool operator==(const CUri& r) const;
         bool operator<(const CUri& r) const;
 
-        //模糊比较。（只比较host和port)
         bool approxEqualsTo(const CUri& r) const; 
 
         virtual ~CUri();
@@ -319,7 +255,7 @@ class CUri
         void  setPort(const int);
         void  setSessionId(const char*);
 
-        const char* c_str() const; //获得其完整string
+        const char* c_str() const; 
 
     private:
         char mHost[128];
@@ -333,13 +269,11 @@ class CTcpConnection : public CSocket
         CTcpConnection();
         ~CTcpConnection();
 
-        //localPort为0时，由操作系统选定本地端口；否则，使用此端口作为客户端端口
         bool connect(const char* remoteHost, int remotePort, char* localHost, int localPort, BOOL needFindHead=FALSE);
         void setFd(int fd); 
         void buildFdSet(CFdSet&);
         int process(CFdSet&);
 
-        // derived class should implement these two function
         virtual void processSend(); 
         virtual int processRecv(); 
 
@@ -396,11 +330,11 @@ class CTcpConnectionManager
         CTcpConnection* findByLinkId(UINT linkId);
         CTcpConnection* findByConnId(UINT connid);
 
-        bool close(const CUri &remoteUri); //关闭一个connection，且delete之。
+        bool close(const CUri &remoteUri); 
         bool closeByLinkId(UINT linkId);
         bool closeByConnId(UINT connId);
         void closeConnectionsWithProblem();
-        void closeAll(); //关闭所有Connection,且移除它们
+        void closeAll(); 
 
         void printState(CStr& str);
         void printConnectionsState(CStr& str);
--- include/comserv/udpsocket.h
+++ include/comserv/udpsocket.h
@@ -8,11 +8,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h> //include inet_addr()  
+#include <arpa/inet.h>  
 #include <netdb.h>
 #include <unistd.h>
 #else
-#include <winsock2.h> //include timeval
+#include <winsock2.h> 
 #endif
 
 #include <fcntl.h>
@@ -36,47 +36,24 @@ class CUDPSocket
     public:
         CUDPSocket();
         virtual ~CUDPSocket();
-
-        //注意，server和client用的是相同的fd。即发送和接收用使用相同的fd,避免干扰。
-        //如果既需要收，也需要发，那么openServer即可。如果只需要发，则openClient。不能同时openServer和openClient
-
-        //打开服务端socket
         INT    openServer(CHAR* localAddr, INT localPort); 
-        //打开客户端socket
         BOOL   openClient(CHAR* remoteAddr,INT remotePort); 
 
-
-        //Added by Lxm. 2009-8-10
-        //打开客户端socket, 不指定对端地址。（每次发送时， 填写地址，以发往不同地址)
         BOOL   openClient();
 
-        //发送消息
-        //-1：写socket失败
-        //-3: socket关闭
-        // 1：成功
         INT     sendCode(CCode &code);   
 
-        INT     sendCode(CCode &code,CHAR* remoteAddr,INT remotePort); //指定对方地址和端口发送  
+        INT     sendCode(CCode &code,CHAR* remoteAddr,INT remotePort);   
 
-        //接收消息
-        //-1: 读Socket失败
-        //-3: socket关闭
-        //-4: code.content==NULL，
-        //    调用Recv()之前未给code.content分配空间
-        // 1: 成功
-        INT     recvCode(CCode& code);   
+	INT     recvCode(CCode& code);   
 
-        //关闭socket
         BOOL    closeSocket();
 
-        INT     getFd() { return m_socketFd;} //最近一次调用open获得的fd
+        INT     getFd() { return m_socketFd;} 
 
-        //得到客户端地址,在调用recvCode之后再调用本方法。
-        //Added by Lxm. 2009-08-10
         sockaddr_in& getClientSockAddr() {	return m_clientAddr;	}
         sockaddr_in& getRemoteSockAddr() { return m_remoteAddr;	}
 
-        // format of addr: xxx.xxx.xxx.xxx:port
         void setRemoteSockAddr(const char* addr);
         void setRemoteSockAddr(const char* host, int port);
         void clientInfo(CStr*);
@@ -84,15 +61,11 @@ class CUDPSocket
 
         void    setAddrReuse(BOOL isAddrReuse) { m_addrReuseFlag = isAddrReuse;}
 
-        //added by Long Xiangming. 2010.1.27
-        //socket buff 可以设置。目前仅使用setRecvBufSize
-        //注意，这两个方法需要显式的调用而不会隐含在openServer或openClient方法中，(在调用openServer方法之后调用)
-        //因为对于心跳等使用udpSocket的地方，并不需要设置buffSize
-        void setRecvBufSize(INT iRecvBufSize);//设置接收缓冲区大小
-        void setRecvBufSize();//设置接收缓冲区大小. 使用默认
+	void setRecvBufSize(INT iRecvBufSize);
+        void setRecvBufSize();
 
-        void setSendBufSize(INT iSendBufSize);//设置发送缓冲区大小
-        void setSendBufSize();//设置发送缓冲区大小,使用默认
+        void setSendBufSize(INT iSendBufSize);
+        void setSendBufSize();
 
     protected:
         struct  sockaddr_in m_localAddr;
@@ -107,17 +80,12 @@ class CUDPSocket
 
         TCommState          m_socketState;
 
-        BOOL				m_addrReuseFlag; //地址重用标记。 //added by Long Xiangming.2006.06.30
-        //缺省为0
+        BOOL				m_addrReuseFlag; 
 
-        //added by Long Xiangming. 2010.1.27
-        //socket buff 可以设置。目前仅使用recvBufSize
         UINT m_socketSendBufSize;
         UINT m_socketRecvBufSize;
 };
 
 
 #endif
-
-
 
